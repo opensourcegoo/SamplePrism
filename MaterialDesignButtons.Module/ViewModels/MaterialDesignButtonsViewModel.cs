0963d0be20afb2adf0c05f85ee45c9df22087241
bianyunpeng <bianyunpeng@qq.com>
2026-01-18 23:28:06 +0800

    增加badge+button

--- MaterialDesignButtons.Module/ViewModels/MaterialDesignButtonsViewModel.cs
+++ MaterialDesignButtons.Module/ViewModels/MaterialDesignButtonsViewModel.cs
@@ -6,11 +6,54 @@ using System.Threading.Tasks;
 
 namespace MaterialDesignButtons.Module.ViewModels
 {
-    public class MaterialDesignButtonsViewModel
+    public class MaterialDesignButtonsViewModel : BindableBase
     {
+        #region Property
+        private int _badgeClickMeCount;
+        public int BadgeClickMeCount
+        {
+            get { return _badgeClickMeCount; }
+            set { SetProperty(ref _badgeClickMeCount, value); }
+        }
+
+        private int _incrementOrClickMeCount;
+        public int IncrementOrClickMeCount
+        {
+            get { return _incrementOrClickMeCount; }
+            set { SetProperty(ref _incrementOrClickMeCount, value);}
+        }
+
+
+        #endregion
+
+        #region Command
+        public DelegateCommand ClickMeCommand { get; set; }
+
+        public DelegateCommand IncrementOrClickMeCommand { get; set; }
+        #endregion
+
+        #region Constructor
         public MaterialDesignButtonsViewModel()
         {
-
+            ClickMeCommand = new DelegateCommand(OnClickMe);
+            IncrementOrClickMeCommand = new DelegateCommand(OnIncrementOrClickMe);
+            BadgeClickMeCount = 0;
+            IncrementOrClickMeCount = 0;
         }
+        #endregion
+
+        #region Method
+        private void OnClickMe()
+        {
+            BadgeClickMeCount++;
+            
+        }
+
+        private void OnIncrementOrClickMe()
+        {
+            IncrementOrClickMeCount++;
+        }
+        #endregion
+
     }
 }
